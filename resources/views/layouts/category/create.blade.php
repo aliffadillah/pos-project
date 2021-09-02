ed526ddb591c0a258fe5d64dc193c76d04ed250a
leopratama <wintsold123@gmail.com>
2021-09-03 00:49:21 +0700

    membuat project pos sampai episode 6

---
+++ resources/views/layouts/category/create.blade.php
@@ -0,0 +1,17 @@
+@extends('master');
+
+@section('title')
+    <title>Category Page</title>
+@endsection
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Add Category</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item">
+                    <a href="#">Home</a>
+                </div>
+            </div>
+    </section>
+@endsection
---
+++ resources/views/layouts/category/index.blade.php
@@ -0,0 +1,53 @@
+@extends('master');
+
+@section('title')
+    <title>Category Page</title>
+@endsection
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Category Page</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item">
+                    <a href="#">Home</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <a href="#" class="btn btn-primary">Add Category</a>
+                    </div>
+                    <div class="card-body">
+                        <div class="table table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <th>No</th>
+                                    <th>Category Name</th>
+                                    <th>Tools</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        @forelse ($category as $c)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{$c->name_category}}</td>
+                                            <td>&nbsp;</td>
+                                        </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="3" class="text-center">Belum ada data</td>
+                                            </tr>
+                                        @endforelse
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
